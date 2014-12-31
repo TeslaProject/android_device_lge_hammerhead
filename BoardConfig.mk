6e7a3644dedcfbd53a2a0930cc9d360dbf8bc897
Randy DuCharme <radio.ad5gb@gmail.com>
2014-12-31 00:48:19 -0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -122,7 +122,7 @@ TARGET_GCC_VERSION_ARM := 4.9-linaro
 # Graphite optimizations
 SYMMETRY := true
 GRAPHITE_OPTS := true
-STRICT_ALIASING := false
+STRICT_ALIASING := true
 
 # The list below is order dependent
 BOARD_SEPOLICY_UNION += \
