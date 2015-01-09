c309008a10ae2c5f1fbe7d13b526d65904a45a40
Randy DuCharme <radio.ad5gb@gmail.com>
2015-01-08 23:16:11 -0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,7 +113,8 @@ BOARD_SEPOLICY_DIRS += \
        device/lge/hammerhead/sepolicy
 
 # Define kernel config for inline building
-TARGET_KERNEL_CONFIG := wicked_hammerhead_defconfig
+##TARGET_KERNEL_CONFIG := wicked_hammerhead_defconfig
+TARGET_KERNEL_CONFIG := faux_hammerhead_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
 
 # Custom Kernel Toolchain
