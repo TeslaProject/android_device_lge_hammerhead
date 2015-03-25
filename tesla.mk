11c126ab248f0593fa150b738b3d42e25ad56ed2
Randy <randy.ducharme.@quikq.com>
2015-03-25 12:46:17 -0500

    Starting up 5.1 work
    Signed-off-by: Randy <randy.ducharme.@quikq.com>

--- tesla.mk
+++ tesla.mk
@@ -4,22 +4,22 @@
 ##TARGET_BOOTANIMATION_HALF_RES := true
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/validus/config/common_full_phone.mk)
+$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hammerhead
-PRODUCT_NAME := validus_hammerhead
+PRODUCT_NAME := tesla_hammerhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT=google/hammerhead/hammerhead:5.0.1/LRX22C/1602158:user/release-keys PRIVATE_BUILD_DESC="hammerhead-user 5.0.1 LRX22C 1602158 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT=google/hammerhead/hammerhead:5.1.0/LMY47D/1602158:user/release-keys PRIVATE_BUILD_DESC="hammerhead-user 5.1.0 LMY47D 1602158 release-keys"
 
 # Enable Torch
 PRODUCT_PACKAGES += Torch
