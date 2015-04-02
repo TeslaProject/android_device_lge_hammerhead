af104656d2bde6815d51673e9701d50b9c515ade
Randy DuCharme <radio.ad5gb@gmail.com>
2015-04-01 19:24:40 -0500

    Let's get this device going, shall we?

--- tesla.mk
+++ tesla.mk
@@ -4,10 +4,10 @@
 ##TARGET_BOOTANIMATION_HALF_RES := true
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
+#$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
+#$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
@@ -19,7 +19,10 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT=google/hammerhead/hammerhead:5.1.0/LMY47D/1602158:user/release-keys PRIVATE_BUILD_DESC="hammerhead-user 5.1.0 LMY47D 1602158 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+PRODUCT_NAME=hammerhead \
+BUILD_FINGERPRINT=google/hammerhead/hammerhead:5.1/LMY47D/1743759:user/release-keys \
+PRIVATE_BUILD_DESC="hammerhead-user 5.1 LMY47D 1743759 release-keys"
 
 # Enable Torch
 PRODUCT_PACKAGES += Torch
